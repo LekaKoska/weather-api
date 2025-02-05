608eb3a1e8735d5fb23aca3363ea34a27f8d6a5e
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-02-05 10:26:09 +0100

    * Add bootstrap and font awesome icons

--- resources/views/weather/addForecast.blade.php
+++ resources/views/weather/addForecast.blade.php
@@ -1,40 +1,71 @@
+@extends("admin.layoutAdmin")
 @php use App\Models\CitiesModel;use App\Models\ForecastModel; @endphp
+
     <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Document</title>
 </head>
 <body>
+@section("content")
 <form action="{{route("forecast.add")}}" method="POST">
     {{csrf_field()}}
-    <select name="cityName">
-        @foreach(CitiesModel::all() as $cityName)
-            <option value="{{$cityName->id}}">{{$cityName->name}}</option>
-        @endforeach
-    </select>
-    <input type="number" name="temperature" placeholder="Unesite temperaturu">
-    <select name="weatherType">
-       @foreach(ForecastModel::WEATHER as $weather)
-            <option>{{$weather}}</option>
-       @endforeach
-    </select>
-    <input type="text" name="chance" placeholder="Sanse za padavinu">
-    <input type="date" name="forecastDate">
-    <button>Dodaj</button>
+    @if($errors->any())
+        {{dd($errors->all())}}
+    @endif
+    <div class="mb-3 col-2">
+        <select name="cityName" class="form-select">
+            @foreach(CitiesModel::all() as $cityName)
+                <option value="{{$cityName->id}}">{{$cityName->name}}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="mb-3 col-2">
+        <input type="number" name="temperature" placeholder="Unesite temperaturu" class="form-control">
+    </div>
+
+    <div class="mb-3 col-2">
+        <select name="weatherType" class="form-select">
+            @foreach(ForecastModel::WEATHER as $weather)
+                <option>{{$weather}}</option>
+            @endforeach
+        </select>
+    </div>
+<div class="mb-3 col-2">
+    <input type="text" name="chance" placeholder="Sanse za padavinu" class="form-control">
+</div>
+<div class="mb-3 col-2">
+    <input type="date" name="forecastDate" class="form-control">
+</div>
+
+    <button class="btn btn-outline-primary col-2 ">Dodaj</button>
 </form>
+<div class="d-flex flex-wrap" style="gap: 10px">
+
         @foreach(CitiesModel::all() as $city)
-            <h4>{{$city->name}}</h4>
-                <ul>
+                <div>
+                <h4 class="mb-1">{{$city->name}}</h4>
+
+                <ul class="list-group mb-4">
                         @foreach($city->forecast as $forecast)
-                           <li>{{$forecast->forecast_date}} - {{$forecast->temperature}} </li>
-                        @endforeach
+                            @php
+                            $boja = \App\Http\ForecastHelper::getColorByTemperature($forecast->temperature);
+                            $ikonica = \App\Http\ForecastHelper::getIconByWeatherType($forecast->weather_type);
+                            @endphp
+
+                           <li class="list-group-item">{{$forecast->forecast_date}} -
+                              <i class="fa-solid {{$ikonica}}"></i>
+                               <span style="color: {{$boja}};">{{$forecast->temperature}} {{$ikonica}}  </span></li>
                 </ul>
+                        @endforeach
+                </div>
         @endforeach
+</div>
 
 
-
-
+@endsection
 </body>
 </html>
