7e20edadd7ad688e46e7c1a4d37d3ec14358d9ed
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-06 21:45:36 +0200

    * using Auth facades into middleware

--- app/Http/Middleware/AdminCheckMiddleware.php
+++ app/Http/Middleware/AdminCheckMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminCheckMiddleware
@@ -16,7 +17,7 @@ class AdminCheckMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $role = Auth::user()->role;
-        if($role != "admin") 
+        if($role != "admin")
         {
             return redirect ("/");
         }
