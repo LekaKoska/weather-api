8007e748c4748a843e2e7eeba6418af8609fcbc0
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-07 12:10:14 +0200

    * Editing blade for search results

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -1,31 +1,60 @@
-@php use App\Http\ForecastHelper;use Illuminate\Support\Facades\Session; @endphp
+@php
+    use App\Http\ForecastHelper;
+    use Illuminate\Support\Facades\Session;
+@endphp
+
 @extends("layout")
 
 @section("pageSection")
-    @if(Session::has("error"))
-        <p class="text-danger">{{Session::get("error")}}</p>
-    @endif
-    <div class="d-flex flex-wrap container">
 
-        @foreach($cities as $city)
+    <div class="container py-5">
 
-            @php
-                $icon = ForecastHelper::getIconByWeatherType($city->todayForecast->weather_type);
-            @endphp
+        @if(Session::has("error"))
+            <div class="alert alert-danger text-center">
+                {{ Session::get("error") }}
+            </div>
+        @endif
 
-            <p>
-                @if(in_array($city->id, $userFavourite))
-                <a class="btn btn-success" href="{{route("forecast.unfavourite", ["city" => $city->id ])}}"><i
-                        class="fa-solid fa-heart"></i></a>
-                @else
-                    <a class="btn btn-warning" href="{{route("forecast.favourite", ["city" => $city->id ])}}"><i
-                            class="fa-regular fa-heart"></i></a>
-                @endif
-                <a class=" btn btn-primary text-white m-2"
-                   href="{{route("forecast.permalink", ["city" => $city->name])}}"><i
-                        class="fa-solid {{$icon}}"></i>{{$city->name}}</a>
-            </p>
 
-        @endforeach
+        <div class="row g-4 justify-content-center">
+            @foreach($cities as $city)
+                @php
+                    $icon = ForecastHelper::getIconByWeatherType($city->todayForecast->weather_type);
+                    $isFavourite = in_array($city->id, $userFavourite);
+                @endphp
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="card shadow-sm border-0 h-100 text-center p-3 bg-light">
+
+                        <div class="mb-3">
+                            <i class="fa-solid {{ $icon }} fa-3x text-primary"></i>
+                        </div>
+
+                        <h4 class="fw-bold mb-3">{{ $city->name }}</h4>
+
+
+                        <div class="d-flex justify-content-center gap-2">
+                            @if($isFavourite)
+                                <a href="{{ route('forecast.unfavourite', ['city' => $city->id]) }}"
+                                   class="btn btn-outline-danger">
+                                    <i class="fa-solid fa-heart"></i> Remove
+                                </a>
+                            @else
+                                <a href="{{ route('forecast.favourite', ['city' => $city->id]) }}"
+                                   class="btn btn-outline-warning">
+                                    <i class="fa-regular fa-heart"></i> Favourite
+                                </a>
+                            @endif
+
+                            <a href="{{ route('forecast.permalink', ['city' => $city->name]) }}"
+                               class="btn btn-primary text-white">
+                                <i class="fa-solid fa-circle-info"></i> Details
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
+
 @endsection
