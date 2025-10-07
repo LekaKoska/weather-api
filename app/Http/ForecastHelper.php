68f950b614f84716153440df54c633d2c4a1e51c
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-07 10:31:12 +0200

    * Using match instead of if else

--- app/Http/ForecastHelper.php
+++ app/Http/ForecastHelper.php
@@ -2,56 +2,40 @@
 
     namespace App\Http;
 
+    use App\Enum\WeatherType;
     use App\Models\ForecastModel;
 
     class ForecastHelper
     {
         public static function getColorByTemperature($temperature)
         {
+            switch (true) {
+                case $temperature === 0:
+                    $color = 'lightblue';
+                    break;
+                case $temperature >= 1 && $temperature <= 10:
+                    $color = 'blue';
+                    break;
+                case $temperature >= 11 && $temperature <= 25:
+                    $color = 'green';
+                    break;
+                default:
+                    $color = 'red';
+            }
 
-            if($temperature <= 0)
-            {
-                $boja = "lightblue";
-            }
-            else if($temperature >= 1 && $temperature <= 10)
-            {
-                $boja = "blue";
-            }
-            else if($temperature > 10 && $temperature < 25)
-            {
-                $boja = "green";
-            }
-            else
-            {
-                $boja = "red";
-            }
-            return $boja;
+            return $color;
+
         }
 
         public static function getIconByWeatherType($type)
         {
-
-
-            if($type == "sunny")
-            {
-                $icon = "fa-sun";
-            }
-            else if($type == "snowy")
-            {
-                $icon = "fa-snowflake";
-            }
-            else if($type == "rainy")
-            {
-                $icon = "fa-cloud-rain";
-            }
-            else if($type == "cloudy")
-            {
-                $icon = "fa-cloud";
-            }
-
-            return $icon;
-
-
+            return match ($type) {
+                WeatherType::SUNNY->value => 'fa-sun',
+                WeatherType::SNOW->value  => 'fa-snowflake',
+                WeatherType::RAIN->value  => 'fa-cloud-rain',
+                WeatherType::CLOUD->value => 'fa-cloud',
+                default  => 'fa-cloud',
+            };
 
         }
     }
